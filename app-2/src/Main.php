076fc4c63268428a80256ec266d499c618e4f84e
crwgregory <crwgregory@gmail.com>
2016-12-11 16:01:42 -0700

    inital commit

---
+++ app-2/src/Main.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Greg Christopherson
+ * 12/11/2016
+ * c.gregory@matter6.com
+ * crwgregory@gmail.com
+ */
+
+namespace app2;
+
+
+class Main
+{
+
+    /**
+     * Main constructor.
+     */
+    public function __construct()
+    {
+        echo 'hello world from app 2';
+    }
+}
